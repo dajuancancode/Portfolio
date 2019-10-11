3f1d1e98c338c39cd8ca2fbbe8fd9233c4be49a8
DaJuan Harris <dajuancancode@gmail.com>
2019-10-11 13:46:36 -0400

    Styled about component and added content

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,16 +1,54 @@
 import React from 'react';
-import { StyledAbout } from './About.styled'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCheck } from '@fortawesome/free-solid-svg-icons';
+
+import { StyledAbout } from './About.styled';
+
 
 const About = () => {
     return (
         <StyledAbout>
             <h1>About</h1>
-            <div>
-                <ul>
-                    <li>JavaScript</li>
-                    <li>CSS</li>
-                </ul>
+            <div className="aboutContent">
+                <div className="aboutParagraphs">
+                    <p>
+                    My name is DaJuan Harris and I am a full stack web and mobile developer from Miami Florida. I have experience in working with a variety of tools and languages to build lightweight applications that are also perform well across many different devices.
+                    </p>
+                    <p>
+                    Creating applications is something that I am extremely passionate about, and I always find myself hungry to learn more about the industry. This allows me to quickly adopt new languages and technologies that can enhance anything I put my hand to.
+                    </p>
+                    <p>
+                    I’m also a student at Florida International University, where I am studying to obtain a degree in Computer Science. After graduating, I plan to enter the workforce, or start my own business.
+                    </p>
+                </div>
+
+                <div className="skillsContent">
+                    <div className="border"></div>
+                    <div className="skillContainer">
+                        <p>Languages</p>
+                        <ul>
+                            <li>HTML</li>
+                            <li>CSS</li>
+                            <li>JavaScript</li>
+                            <li>Python</li>
+                            <li>Java</li>
+                            <li>C++</li>
+                        </ul>
+                    </div>
+                    <div className="skillContainer">
+                        <p>Frameworks &amp; Tools</p>
+                        <ul>
+                            <li>Git / Github</li>
+                            <li>React</li>
+                            <li>React Native</li>
+                            <li>Flask</li>
+                            <li>Node JS</li>
+                            <li>Express</li>
+                        </ul>
+                    </div>
+                </div>
             </div>
+            
         </StyledAbout>
     )
 }
--- src/components/About/About.styled.js
+++ src/components/About/About.styled.js
@@ -1,26 +1,87 @@
 import styled from 'styled-components';
 
+
 export const StyledAbout = styled.section`
     background: #f5f6fb;
     grid-area: 3 / 1 / span 2 / -1
     display: flex;
     flex-direction: column;
     justify-content: space-between;
+    font-weight: 300;
+    font-size: 1.8rem;
+    line-height: 1.5;
     
 
     h1 {
         font-size: 3rem
-        justify-self: center;
-        border-bottom: 2px solid ${({theme}) => theme.primaryAccent};
+        align-self: center;
     }
 
-    div {
-       width: 95%;
-       margin: 10px auto 10px;
-       height: auto;
-       background-color: ${({theme}) => theme.primaryLight};
-       border: transparent;
-       border-radius: 3px;
-       box-shadow: 0 4px 6px 0 rgba(29,25, 92, 0.25)
+    .aboutContent {
+        display: flex;
+        margin-bottom: 1rem;
+
+        .aboutParagraphs {
+            display: flex;
+            flex-direction: column;
+            width: 70%;
+            
+            p {
+                padding: 1rem;
+            }
+
+
+        }
+       
+
+       .skillsContent {
+            height: auto;
+            background-color: ${({theme}) => theme.primaryLight}
+            margin: 0 auto;
+            box-shadow: 0 3rem 6rem rgba(0, 0, 0, 0.16);
+            display: flex
+            flex-direction: column;
+            border-radius: 3px;
+
+            .border {
+                height: 5px;
+                background-color: ${({theme}) => theme.primaryAccent};
+                margin-bottom: 2rem;
+                border-radius: 3px 3px 0 0;
+            }
+
+            p {
+                align-self: center;
+            }
+
+            .skillContainer{
+                display: flex;
+                flex-direction: column;
+                flex-wrap: wrap;
+                width: 95%;
+                margin: .5rem auto;
+                justify-content: center;
+
+                ul {
+                    margin: 0 auto;
+                    margin-top: 1.5rem;
+                    li {
+                        list-style: none;
+                        padding-bottom: 0.3rem;
+                    }
+                }
+
+            }
+
+            @media (min-width: 768px) {
+                width:  25%
+            }
+        }
+
+        @media (min-width: 768px) {
+            flex-direction: row;
+        }
     }
+
+    
 `
